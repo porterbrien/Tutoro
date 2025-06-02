b5f371ae48103a492df4198ee3123827c9b76c16
porterbrien <porterbrien@icloud.com>
2025-06-02 11:52:37 -0600

    Express and backend

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3001;
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.send('API running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
