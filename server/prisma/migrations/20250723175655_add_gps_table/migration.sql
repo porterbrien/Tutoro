df93b30e8596a2a2b8ac0e54db45204faaae3ad5
porterbrien <porterbrien@icloud.com>
2025-07-23 11:58:31 -0600

    times 2. improper increment

---
+++ server/prisma/migrations/20250723175655_add_gps_table/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `gps_saved_location` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `account` DROP FOREIGN KEY `fk_account_gps1`;
+
+-- DropTable
+DROP TABLE `gps_saved_location`;
+
+-- CreateTable
+CREATE TABLE `gps` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `latitude` VARCHAR(191) NOT NULL,
+    `longitude` VARCHAR(191) NOT NULL,
+    `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `account` ADD CONSTRAINT `fk_account_gps1` FOREIGN KEY (`gps_gps_id`) REFERENCES `gps`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
